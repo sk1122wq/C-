bf8fd5e89c4a8ef19a1333d6cd4c2e083fbf0049
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2026-01-04 09:12:08 +0500

    Add files via upload

---
+++ Alphabet - Copy.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<<"enter any character";
+	cin>>ch;
+	if((ch>='a'&& ch<='z')|| (ch>='A'&& ch<='Z')){
+	
+	cout<<ch<<"=is an alphabet";}
+	else if((ch>'0'&&ch<='9'))
+	{
+		cout<<ch<<"=is an digit";
+	}
+	else{
+		cout<<ch<<"= is a special character";
+	}
+	return 0;
+	
+}
