b9b93453a27173a6b5e378a50a69b41f1a47e289
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2026-01-06 08:29:05 +0500

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio.h>
+class CRectangle
+{
+	private:
+		int X,Y;
+		public:
+		int main (int a, int b)
+			{
+				X=a;
+				Y=b;
+			}
+			int area()
+			{
+				cout<<"area of the rectangle="<<(X*Y);
+			};
+			void main(){
+				CRectangle R1;
+				R1. set-value(44,22);
+				R1.area();
+				getch();
+				Area of triangle;
+			}
+}
