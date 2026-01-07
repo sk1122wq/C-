ba7f8e9f9706c4a267e8c367c90a20c3290c3ccc
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2026-01-07 09:08:07 +0500

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,13 @@
+/*  1 write a program that reads three value also find the average of the three numbers.
+    2 write a program of c++, if the value of variable  is greater or than equal to 1000 then program 
+    execute otherwise not... */
+    #include<iostream>
+    using namespace std;
+    int main()
+    {
+    	int x,y,z,average;
+    	cin>>x>>y>>z;
+    	average=x+y+z/2;
+    	cout<<"Average is"<<average;
+	}
+    
