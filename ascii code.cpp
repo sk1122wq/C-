7e16ba7b855077a201c177bbffc7f76ae38dc328
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2026-01-07 09:06:42 +0500

    Add files via upload

---
+++ ascii code.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//ascii code
+	char  ascii;
+	cout<<"enter the alphabat";
+	cin>>ascii;
+	if(ascii>=65&&ascii<=90)
+	cout<<"upper casr";
+	else
+	cout<<"lower case";
+}
