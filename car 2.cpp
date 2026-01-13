b21900f9120a48a3c12cfde6decbac33138a74bd
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2026-01-13 08:35:48 +0500

    Add files via upload

---
+++ car 2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class car2{
+	private:
+		string model;
+		int year;
+	public:
+		void input(){
+		
+		cout<<"Enter model";
+		cin>>model;
+		cout<<"enter year";
+		cin>>year;
+		}
+		void display()
+		{
+		cout<<"car model"<<model<<"year"<<year<<endl;
+		}
+};
+int main(){
+	car2 Mycar;
+	Mycar.input();
+	Mycar.display();
+	return 0;
+}
