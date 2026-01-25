e7304acb019c48a2d691ad3aa071f153e117a5c2
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2026-01-25 08:56:32 +0500

    Add files via upload

---
+++ exa 19.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=6,y=4,square,square2;
+	
+	square=x*x;
+	cout<<"\nsquare is"<<square;
+	
+	square2=y*y;
+	cout<<"\nsquare2 is"<<square2;
+
+	
+	
+}
