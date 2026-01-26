f1e3808e492c67f26ce73a2ceabaaaf07c5a1c96
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2026-01-26 09:18:54 +0500

    Add files via upload

---
+++ exa 18.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y,z;
+	cout<<"Enter fist number\n";
+	cin>>x;
+	cout<<"Enter second number\n";
+	cin>>y;
+	z=x+y;
+	cout<<"Add="<<z;
+	}
