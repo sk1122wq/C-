41d4989cc875aa1c51e4b5d8cc2391d18251ec0b
Muhammad Ibrahim <samiibrahim1882@gmail.com>
2026-01-29 11:18:23 +0500

    Add files via upload

---
+++ for loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+//for(int initilize;condition;increment/decrement)
+cout<<"welcome"<<endl;
+cout<<"welcome"<<endl;
+cout<<"welcome"<<endl;
+cout<<"welcome"<<endl;
+cout<<"welcome"<<endl;
+cout<<"welcome"<<endl;
+}
